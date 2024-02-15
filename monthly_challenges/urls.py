26034bebca4f9d1f585423cf5bef9b7309007b1e
HDee-hd <hdairo199@gmail.com>
2024-02-15 09:29:39 +0100

    Added new

--- monthly_challenges/urls.py
+++ monthly_challenges/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("challenges", include("challenges.url"))
-    
-]
+    path("challenges/", include("challenges.urls"))
+    ]
